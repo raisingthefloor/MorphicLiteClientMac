50b6e9e6829639ff49025eaef7df5f4103339189
Owen Shaw <owen@owenshaw.com>
2020-04-13 12:21:04 -0700

    rename MorphicMenuBar to Morphic

--- Morphic/MorphicConfigurator/AppDelegate.swift
+++ Morphic/MorphicConfigurator/AppDelegate.swift
@@ -21,29 +21,27 @@
 // * Adobe Foundation
 // * Consumer Electronics Association Foundation
 
-import XCTest
-@testable import MorphicConfigurator
+import Cocoa
+import MorphicService
 
-class MorphicConfiguratorTests: XCTestCase {
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window.contentViewController = ViewController(nibName: "ViewController", bundle: nil)
+        window.makeKeyAndOrderFront(nil)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
+
--- Morphic/MorphicConfiguratorUITests/MorphicConfiguratorUITests.swift
+++ Morphic/MorphicConfiguratorUITests/MorphicConfiguratorUITests.swift
@@ -1,31 +1,16 @@
-// Copyright 2020 Raising the Floor - International
 //
-// Licensed under the New BSD license. You may not use this file except in
-// compliance with this License.
+//  MorphicConfiguratorUITests.swift
+//  MorphicConfiguratorUITests
 //
-// You may obtain a copy of the License at
-// https://github.com/GPII/universal/blob/master/LICENSE.txt
+//  Created by Owen Shaw on 4/13/20.
+//  Copyright © 2020 Raising the Floor. All rights reserved.
 //
-// The R&D leading to these results received funding from the:
-// * Rehabilitation Services Administration, US Dept. of Education under
-//   grant H421A150006 (APCP)
-// * National Institute on Disability, Independent Living, and
-//   Rehabilitation Research (NIDILRR)
-// * Administration for Independent Living & Dept. of Education under grants
-//   H133E080022 (RERC-IT) and H133E130028/90RE5003-01-00 (UIITA-RERC)
-// * European Union's Seventh Framework Programme (FP7/2007-2013) grant
-//   agreement nos. 289016 (Cloud4all) and 610510 (Prosperity4All)
-// * William and Flora Hewlett Foundation
-// * Ontario Ministry of Research and Innovation
-// * Canadian Foundation for Innovation
-// * Adobe Foundation
-// * Consumer Electronics Association Foundation
 
 import XCTest
 
 class MorphicConfiguratorUITests: XCTestCase {
 
-    override func setUp() {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -34,11 +19,11 @@ class MorphicConfiguratorUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
@@ -47,7 +32,7 @@ class MorphicConfiguratorUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() {
+    func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
