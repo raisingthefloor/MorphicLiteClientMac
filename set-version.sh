8935c6f4a6686b4d7328ab3be1040cbf40d74762
DaytonG <dayton.tar.gz@gmail.com>
2020-05-07 15:13:45 -0600

    use args to pass in version

--- set-version.sh
+++ set-version.sh
@@ -5,17 +5,24 @@ set -x
 
 # ref: https://crunchybagel.com/auto-incrementing-build-numbers-in-xcode/
 
-buildNumber=$(date -u "+%Y%m%d%H%M")
+NEW_VERSION=${1}
+
+if [[ "$NEW_VERSION" = "" ]]; then
+  echo "usage: ./set-version [NEW VERSION]"
+  echo "no version provided"
+  exit 1
+fi
+
+echo "new version: ${NEW_VERSION}"
 
 updateVersion() {
   local PLIST=$1
   /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $buildNumber" "$PLIST"  # Version number
   /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$PLIST"  # Build number
-  echo "DateTime for app version number: $buildNumber"
 }
 
 for filepath in $(find . -name 'Info.plist' -type f)
 do
-  echo "Updating ${filepath}"
+  echo "Updating plist: ${filepath}"
   updateVersion "$filepath"
 done
