2f2e437b0e496099791f7f4f51c8942be20b8fd6
Owen Shaw <owen@owenshaw.com>
2020-05-31 14:07:36 -0700

    update dmg script to properly error out if there's an issue

--- Morphic/Morphic/CreateDiskImage.sh
+++ Morphic/Morphic/CreateDiskImage.sh
@@ -6,18 +6,25 @@
 #  Created by Owen Shaw on 5/29/20.
 #  Copyright © 2020 Raising the Floor. All rights reserved.
 
+echo "---- Running CreateDiskImage.sh -----"
+
 TEMPLATE_NAME="MorphicTemplate.dmg"
 MOUNT_PATH="MorphicInstaller"
 APP_NAME="${PRODUCT_NAME}.app"
 COMPRESSED_TEMPLATE_PATH="${SRCROOT}/Morphic/${TEMPLATE_NAME}.bz2"
+TEMP_FOLDER="Morhpic.DiskImage.build"
 
 cd "${CONFIGURATION_TEMP_DIR}"
-rm -rf installer
-mkdir installer
+rm -rf "${TEMP_FOLDER}"
+mkdir "${TEMP_FOLDER}" && echo "[dmg] Created folder ${CONFIGURATION_TEMP_DIR}/${TEMP_FOLDER}" || exit
+cd "${TEMP_FOLDER}" && echo "[dmg] Working in folder ${TEMP_FOLDER}" || exit
 
-bunzip2 -k "${COMPRESSED_TEMPLATE_PATH}" -c > "${TEMPLATE_NAME}"
-hdiutil attach "${TEMPLATE_NAME}" -noautoopen -quiet -mountpoint "${MOUNT_PATH}"
-ditto "${CONFIGURATION_BUILD_DIR}/${APP_NAME}" "${MOUNT_PATH}/${APP_NAME}"
-hdiutil detach "${MOUNT_PATH}" -quiet -force
-hdiutil convert "${TEMPLATE_NAME}" -quiet -format UDZO -imagekey -zlib-level=9 -o "${CONFIGURATION_BUILD_DIR}/${PRODUCT_NAME}.dmg"
-rm "${TEMPLATE_NAME}"
+bunzip2 -k "${COMPRESSED_TEMPLATE_PATH}" -c > "${TEMPLATE_NAME}" && echo "[dmg] unzipped ${TEMPLATE_NAME}" || exit
+hdiutil attach "${TEMPLATE_NAME}" -noautoopen -quiet -mountpoint "${MOUNT_PATH}" && echo "[dmg] mounted ${TEMPLATE_NAME} to ${MOUNT_PATH}" || exit
+ditto "${CONFIGURATION_BUILD_DIR}/${APP_NAME}" "${MOUNT_PATH}/${APP_NAME}" && echo "[dmg] copied ${APP_NAME} to ${MOUNT_PATH}" || exit
+hdiutil detach "${MOUNT_PATH}" -quiet -force && echo "[dmg] unmounted ${MOUNT_PATH}" || exit
+rm -f "${CONFIGURATION_BUILD_DIR}/${PRODUCT_NAME}.dmg"
+hdiutil convert "${TEMPLATE_NAME}" -quiet -format UDZO -imagekey -zlib-level=9 -o "${CONFIGURATION_BUILD_DIR}/${PRODUCT_NAME}.dmg" && echo "[dmg] failed to create ${CONFIGURATION_BUILD_DIR}/${PRODUCT_NAME}.dmg" || exit
+cd ..
+rm -rf "${TEMP_FOLDER}" && echo "[dmg] cleaned up ${TEMP_FOLDER}" || exit
+echo "[dmg] done"
