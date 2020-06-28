106b813aa55afd2c031c74513f0a2990c63b2c02
DaytonG <dayton.tar.gz@gmail.com>
2020-06-28 10:34:29 -0600

    fix date

--- push-to-s3.sh
+++ push-to-s3.sh
@@ -26,21 +26,27 @@ fi
 EXTRA_ARGS=""
 S3_OBJECT_PREFIX=""
 
+# OSX's date util is different than linux. You gotta do a bit more legwork to get an expiry date.
+expiry()
+{
+  date -v "${1}" -u +"%Y-%m-%dT%H:%M:%SZ"
+}
+
 if [[ "${BRANCH_NAME}" == "master" ]]; then
   echo "detected master build"
   S3_OBJECT_PREFIX="osx/edge"
-  EXTRA_ARGS="--expires $(date -d '+21 days' --iso-8601=seconds)"
+  EXTRA_ARGS="--expires $(expiry '+21d')"
 elif [[ "${BRANCH}" == *"staging/"* ]]; then
   echo "detected staging build"
   S3_OBJECT_PREFIX="osx/staging"
-  EXTRA_ARGS="--expires $(date -d '+14 days' --iso-8601=seconds)"
+  EXTRA_ARGS="--expires $(expiry '+14d')"
 elif [[ "${BRANCH}" == *"release/"* ]]; then
   echo "detected release build"
   S3_OBJECT_PREFIX="osx/stable"
 else
   echo "detected PR build"
   S3_OBJECT_PREFIX="osx/internal"
-  EXTRA_ARGS="--expires $(date -d '+2 days' --iso-8601=seconds)"
+  EXTRA_ARGS="--expires $(expiry '+2d')"
 fi
 
 set -e
