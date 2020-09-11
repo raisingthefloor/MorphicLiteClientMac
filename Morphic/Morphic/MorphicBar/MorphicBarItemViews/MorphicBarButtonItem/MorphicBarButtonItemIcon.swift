e70380ae734be93aeda743d7216fc9bf390d6245
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2020-09-11 11:15:50 -0400

    Updated, simplified MorphicBar icon names

--- Morphic/Morphic/MorphicBar/MorphicBarItemViews/MorphicBarButtonItem/MorphicBarButtonItemIcon.swift
+++ Morphic/Morphic/MorphicBar/MorphicBarItemViews/MorphicBarButtonItem/MorphicBarButtonItemIcon.swift
@@ -24,56 +24,25 @@
 import Foundation
 
 enum MorphicBarButtonItemIcon: String {
-    case amazon = "amazon"
-    case calendar = "calendar"
-    case camera = "camera"
-    case comment = "comment"
-    case envelope = "envelope"
-    case google = "google"
-    case googleDrive = "google-drive"
-    case images = "images"
-    case link = "link"
-    case music = "music"
-    case newspaper = "newspaper"
-    case question = "question"
-    case shoppingCart = "shopping-cart"
-    case skype = "skype"
-    case video = "video"
+    case amazon = "amazon-brands"
+    case calendar = "calendar-solid"
+    case camera = "camera-solid"
+    case comment = "comment-solid"
+    case envelope = "envelope-solid"
+    case google = "google-brands"
+    case googleDrive = "google-drive-brands"
+    case images = "images-solid"
+    case link = "link-solid"
+    case music = "music-solid"
+    case newspaper = "newspaper-solid"
+    case question = "question-solid"
+    case shoppingCart = "shopping-cart-solid"
+    case skype = "skype-brands"
+    case video = "video-solid"
     
     var pathToImage: String {
         get {
-            switch self {
-            case .amazon:
-                return Bundle.main.path(forResource: "amazon-brands", ofType: "pdf")!
-            case .calendar:
-                return Bundle.main.path(forResource: "calendar-solid", ofType: "pdf")!
-            case .camera:
-                return Bundle.main.path(forResource: "camera-solid", ofType: "pdf")!
-            case .comment:
-                return Bundle.main.path(forResource: "comment-solid", ofType: "pdf")!
-            case .envelope:
-                return Bundle.main.path(forResource: "envelope-solid", ofType: "pdf")!
-            case .google:
-                return Bundle.main.path(forResource: "google-brands", ofType: "pdf")!
-            case .googleDrive:
-                return Bundle.main.path(forResource: "google-drive-brands", ofType: "pdf")!
-            case .images:
-                return Bundle.main.path(forResource: "images-solid", ofType: "pdf")!
-            case .link:
-                return Bundle.main.path(forResource: "link-solid", ofType: "pdf")!
-            case .music:
-                return Bundle.main.path(forResource: "music-solid", ofType: "pdf")!
-            case .newspaper:
-                return Bundle.main.path(forResource: "newspaper-solid", ofType: "pdf")!
-            case .question:
-                return Bundle.main.path(forResource: "question-solid", ofType: "pdf")!
-            case .shoppingCart:
-                return Bundle.main.path(forResource: "shopping-cart-solid", ofType: "pdf")!
-            case .skype:
-                return Bundle.main.path(forResource: "skype-brands", ofType: "pdf")!
-            case .video:
-                return Bundle.main.path(forResource: "video-solid", ofType: "pdf")!
-            }
+            return Bundle.main.path(forResource: self.rawValue, ofType: "pdf")!
         }
     }
 }
